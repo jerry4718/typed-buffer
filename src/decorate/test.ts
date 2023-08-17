0f9018d8a54210a65e4ccfe326b1071ab2cbbff7
jerry4718 <jerry4718@qq.com>
2023-08-17 23:40:51 +0800

    调试metadata的使用效果，封装两个辅助函数

--- src/decorate/test.ts
+++ src/decorate/test.ts
@@ -2,7 +2,7 @@ import { StructFields, StructParser } from './meta.ts';
 import 'reflect-metadata';
 
 function def() {
-    console.log(arguments);
+	console.log(arguments);
 }
 
 const defineClassDecorator = (decorator: ClassDecorator): ClassDecorator => decorator;
@@ -11,54 +11,59 @@ const defineMethodDecorator = (decorator: MethodDecorator): MethodDecorator => d
 const defineParameterDecorator = (decorator: ParameterDecorator): ParameterDecorator => decorator;
 
 function Parser(name: string) {
-    return defineClassDecorator(Reflect.metadata(StructParser, name));
+	return defineClassDecorator(Reflect.metadata(StructParser, name));
 }
 
 function Field(config: object) {
-    // const fields: any[] = Reflect.hasMetadata(StructFields, klass, propertyKey)
-    //     ? Reflect.getMetadata(StructFields, klass, propertyKey)
-    //     : [];
-    // const meta = Reflect.metadata(StructFields, fields);
-    // const lastIndex = fields.findLastIndex(field => field.name === propertyKey);
-    // const addIndex = lastIndex !== -1 ? lastIndex : fields.length;
-    //
-    // const fieldConfig = { name: propertyKey, slot: false, ...config };
-    // fields.splice(addIndex, 1, fieldConfig);
+	return definePropertyDecorator(function (proto, propertyKey) {
+		const fields: any[] = Reflect.hasOwnMetadata(StructFields, proto)
+			? Reflect.getOwnMetadata(StructFields, proto)
+			: [];
 
-    return definePropertyDecorator(Reflect.metadata(StructFields, config));
-}
+		fields.push({ name: propertyKey, slot: false, ...config });
 
-function Field2(config: object) {
-    return definePropertyDecorator(function (klass, propertyKey) {
-        const fields: any[] = Reflect.hasOwnMetadata(StructFields, klass)
-            ? Reflect.getOwnMetadata(StructFields, klass)
-            : [];
-
-        fields.push({ name: propertyKey, slot: false, ...config });
-
-        Reflect.defineMetadata(StructFields, fields, klass);
-    });
+		Reflect.defineMetadata(StructFields, fields, proto);
+	});
 }
 
 @Parser('Test')
 class Test {
-    /*@Field({ a: 1 }) */@Field2({ a: 1 }) name?: string;
-    /*@Field({ a: 2 }) */@Field2({ a: 2 }) age?: number;
+	@Field({ a: 1 }) name?: string;
+	@Field({ a: 2 }) age?: number;
 }
 
 @Parser('Test2')
 class Test2 extends Test {
-    // @ts-ignore
-    /*@Field({ a: 11 }) */@Field2({ a: 11 }) name?: string;
-    // @ts-ignore
-    /*@Field({ a: 12 }) */@Field2({ a: 12 }) age?: number;
+	@Field({ a: 11 }) declare name?: string;
+	@Field({ a: 12 }) declare age?: number;
+
+	constructor() {
+		super();
+	}
 }
 
-console.log({ ['Test:MetadataKeys']: Reflect.getMetadataKeys(Test), ['Test@@StructParser']: Reflect.getMetadata(StructParser, Test) });
-console.log({ ['Test2:MetadataKeys']: Reflect.getMetadataKeys(Test2), ['Test2@@StructParser']: Reflect.getMetadata(StructParser, Test2) });
+function getClassMetadata<T>(klass: new () => T, metadataKey: string | symbol) {
+	const result = [];
+	let cur = klass;
+    const judgeRoot = Object.getPrototypeOf(Function)
+	while (cur !== judgeRoot) {
+		result.push(Reflect.getMetadata(metadataKey, cur));
+		cur = Object.getPrototypeOf(cur);
+	}
+	return result;
+}
 
-const test = Reflect.construct(Test, []);
-const test2 = Reflect.construct(Test2, []);
+function getProtoMetadata<T>(klass: new () => T, metadataKey: string | symbol) {
+	const result = [];
+	let cur = klass.prototype;
+	while (cur.constructor !== Object) {
+		result.push(Reflect.getMetadata(metadataKey, cur));
+		cur = Object.getPrototypeOf(cur);
+	}
+	return result;
+}
 
-console.log({ ['test:MetadataKeys']: Reflect.getMetadataKeys(test), ['test@@StructFields']: Reflect.getMetadata(StructFields, test) });
-console.log({ ['test2:MetadataKeys']: Reflect.getMetadataKeys(test2), ['test2@@StructFields']: Reflect.getMetadata(StructFields, test2) });
+console.log({
+	'Test2::ClassMetadata@@StructParser': getClassMetadata(Test2, StructParser),
+	'Test2::FieldMetadata@@StructFields': getProtoMetadata(Test2, StructFields),
+});
