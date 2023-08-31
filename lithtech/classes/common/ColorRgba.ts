b00a0e4db2c15c5f7d96606b0042381495b1a731
jerry4718 <jerry4718@qq.com>
2023-08-31 18:29:16 +0800

    1.一些性能比较的测试 2.优化TypeUtil....

--- lithtech/classes/common/ColorRgba.ts
+++ lithtech/classes/common/ColorRgba.ts
@@ -2,7 +2,7 @@ import * as t from '../../../mod.ts';
 import { FieldType, ParserTarget } from '../../../mod.ts';
 
 @ParserTarget()
-export class ColorRgba {
+export class Uint8ColorRgba {
     @FieldType(t.Uint8)
     r!: number;
 
@@ -19,3 +19,57 @@ export class ColorRgba {
         return (this.r << 24) & (this.g << 16) & (this.b << 8) & this.a;
     }
 }
+
+@ParserTarget()
+export class HexColorRgba {
+    @FieldType(t.Uint32)
+    private hex!: number;
+
+    get r() {
+        return this.hex >>> 24 && 0xff;
+    }
+
+    get g() {
+        return this.hex >>> 16 && 0xff;
+    }
+
+    get b() {
+        return this.hex >>> 8 && 0xff;
+    }
+
+    get a() {
+        return this.hex >>> 0 && 0xff;
+    }
+
+    get originHex() {
+        return this.hex;
+    }
+}
+
+@ParserTarget()
+export class ArrayColorRgba {
+    @FieldType(t.Uint8Array, { count: 4 })
+    private meta!: Uint8Array;
+
+    get r() {
+        return this.meta.at(0)!;
+    }
+
+    get g() {
+        return this.meta.at(1)!;
+    }
+
+    get b() {
+        return this.meta.at(2)!;
+    }
+
+    get a() {
+        return this.meta.at(3)!;
+    }
+
+    get originHex() {
+        return (this.r << 24) & (this.g << 16) & (this.b << 8) & this.a;
+    }
+}
+
+export { HexColorRgba as ColorRgba };
--- lithtech/classes/common/Vector3.ts
+++ lithtech/classes/common/Vector3.ts
@@ -1,14 +1,51 @@
 import * as t from '../../../mod.ts';
 import { FieldType, ParserTarget } from '../../../mod.ts';
 
+// @ParserTarget()
+// export class Vector3 {
+//     @FieldType(t.Float32)
+//     x!: number;
+//
+//     @FieldType(t.Float32)
+//     y!: number;
+//
+//     @FieldType(t.Float32)
+//     z!: number;
+// }
+
 @ParserTarget()
 export class Vector3 {
-    @FieldType(t.Float32)
-    x!: number;
+    @FieldType(t.Float32Array, { count: 3 })
+    private meta!: Float32Array;
 
-    @FieldType(t.Float32)
-    y!: number;
+    get x() {
+        return this.meta.at(0)!;
+    }
 
-    @FieldType(t.Float32)
-    z!: number;
+    set x(x: number) {
+        this.ensureMeta();
+        this.meta[0] = x;
+    }
+
+    get y() {
+        return this.meta.at(1)!;
+    }
+
+    set y(y: number) {
+        this.ensureMeta();
+        this.meta[1] = y;
+    }
+
+    get z() {
+        return this.meta.at(2)!;
+    }
+
+    set z(z: number) {
+        this.ensureMeta();
+        this.meta[2] = z;
+    }
+
+    ensureMeta() {
+        if (!this.meta) this.meta = Float32Array.of(0, 0, 0);
+    }
 }
